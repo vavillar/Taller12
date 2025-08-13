36054c77ffd22a9e43436fcf5cd2d45a69b20793
Danx111 <joselyne200509@gmail.com>
2025-08-13 16:41:46 -0500

    refactorizacion de buscaEmpleado

--- Taller Refactoring/src/BuscadorEmpleado.java
+++ Taller Refactoring/src/BuscadorEmpleado.java
@@ -1,14 +1,9 @@
-import java.util.List;
 
 public class BuscadorEmpleado {
-    public static Empleado buscarEmpleadoPorNombre(String nombre, List<Empleado> empleados) {
-        for (Empleado empleado : empleados) {
-            if (empleado.getNombre().equals(nombre)) {
-                return empleado;
-            }
-        }
-        return null;
+    public Optional<Empleado> buscarEmpleadoPorNombre(String nombre) {
+        return empleados.stream()
+            .filter(e -> e.getNombre().equals(nombre))
+            .findFirst();
     }
-
-    // Más metodos
 }
+
